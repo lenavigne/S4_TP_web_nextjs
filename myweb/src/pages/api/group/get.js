96b8af8d2f99643c9e6fc6fa51f8abd5aa17bf26
lenavigne <lena.ferrer.vigne@gmail.com>
2023-03-29 22:51:07 +0200

    v5 29/03

---
+++ myweb/src/pages/api/group/get.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import React from 'react';
+import { getSession } from 'next-auth/react';
+
+const prisma = new PrismaClient();
+
+export default async function sertarien(req, res) {
+    const session = await getSession({req});
+
+    if(!session) {
+        return res.status(418).json({message: 'Im a teapot!'});
+    }
+
+    const current_user = await prisma.user.findUnique({
+        where: { email: session.user.email },
+        include: { group: true }
+    });
+
+    return res.status(200).json(current_user.group);
+}
--- myweb/src/pages/index.jsx
+++ myweb/src/pages/index.jsx
@@ -1,20 +1,53 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useState } from 'react';
+import React from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Component() {
   const { data: session } = useSession()
+  const [list_gr, setListGr] = useState([])
+  const [inputValue, setInputValue] = useState();
+
+
+  const handleInputChange = (event) => { setInputValue(event.target.value); }
+
+  const getGr = async () => {
+    const resp = await fetch('/api/group/get', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+    });
+    const data = await JSON.stringify(resp);
+    console.log("Data : ",data);
+    setListGr(data);
+    console.log("ETETETTTEETE : ",list_gr);
+    if (!resp.ok) {
+      throw new Error('Error! status: ${resp.status}');
+    }
+  };
+
+
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
         <button><a href="./group">Add group</a></button>
+        <div>
+          <div>
+            <h1>Liste de vos groupes : </h1>
+            <button onClick={getGr}>&#128257;</button>
+          </div>
+          <div>
+            
+              {list_gr.map((item) => {
+                return (
+                  <div>
+                    <a href={"/group/" + item.id}>{item.name}</a>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
       </>
     )
   }
